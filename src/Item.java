47a515174cb0d59119e2bb3f0d322217d80eb96c
Rasmus <63317079+Razberryjam@users.noreply.github.com>
2020-10-21 15:41:31 +0200

    Update Item.java
    Added attributes and get/setter

--- src/Item.java
+++ src/Item.java
@@ -1,6 +1,35 @@
 
 public class Item {
-
+	
+	protected int id;
+	protected String name;
+	protected int value;
+	
+	
+	
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+	
 	
 
 }
