13f28206c4beb7c0c5503035a766eff8567f8fbb
Ado-wq <69817904+Ado-wq@users.noreply.github.com>
2020-10-21 15:42:47 +0200

    Update Book.java
    Added atrubutred and getters and setters, and extended to items

--- src/Book.java
+++ src/Book.java
@@ -1,4 +1,19 @@
 
-public class Book {
+public class Book extends Item {
+	private int totalPages;
+	private String publisher;
+	public int getTotalPages() {
+		return totalPages;
+	}
+	public void setTotalPages(int totalPages) {
+		this.totalPages = totalPages;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	
 
 }
